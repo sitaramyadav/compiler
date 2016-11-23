096b6610d68789a3cdb7028676144e41489d8acd
sitaram <sitaram@thoughtworks.com>
2016-11-23 12:16:16 +0530

    @sitaram:@sitaram: resoloved the conflicts.

---
+++ src/js/grammer.js
@@ -0,0 +1,2 @@
+var converter = require('number-to-words');
+
---
+++ src/js/tree.js
@@ -0,0 +1,12 @@
+class Tree {
+    constructor(height, width) {
+
+    }
+
+    parenthesize() {
+        console.log(this.name + ' makes a noise.');
+    }
+    toWords () {
+        return
+    }
+}
